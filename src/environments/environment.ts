bf0eb4cef84a9568fb676cc63fc6761183a9eaf1
Jali Mnisi <mnisi@thedigitalacademy.co.za>
2021-09-15 16:51:35 +0200

    changed the url to the online one

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://localhost:3000/api/phonebook',
+  url: 'https://phone-book-backendj.herokuapp.com/api/phonebook',
   //https://phone-book-backendj.herokuapp.com/api/phonebook
 };
 
